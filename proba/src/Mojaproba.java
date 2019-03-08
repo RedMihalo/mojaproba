adf06820f43b71ab037e47272d78101e23cfcf3f
Michal <209333@edu.p.lodz.pl>
2019-03-08 09:18:19 +0100

    Commit ma byc na branch 02

--- proba/src/Mojaproba.java
+++ proba/src/Mojaproba.java
@@ -1,7 +1,7 @@
 public class Mojaproba {
 
     public static void main(String[]args){
-        System.out.println("W pierwszym branchu; 2+3="+Policz(2,3));
+        System.out.println("W pierwszym branchu; 5+6="+Policz(5,6));
     }
     public static int Policz(int raz,int dwa){
         return raz+dwa;
