25fd387e42b48a02b8960854e29c09ded3ec8cb8
RedMihalo <209333@edu.p.lodz.pl>
2019-03-13 16:36:20 +0100

    zmiana 2

--- proba/src/Mojaproba.java
+++ proba/src/Mojaproba.java
@@ -1,9 +1,9 @@
 public class Mojaproba {
 
     public static void main(String[]args){
-        System.out.println("W pierwszym branchu; 5+6="+Policz(5,6));
+        System.out.println("W pierwszym branchu; 5*6="+Policz(5,6));
     }
     public static int Policz(int raz,int dwa){
-        return raz+dwa;
+        return raz*dwa;
     }
 }
